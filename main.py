678ad8f218334c1ba4c8256554ce1497fee3e1f2
info1 <xrouleau01@gmail.com>
2024-09-25 10:17:53 -0400

    Arrangé la validation des % de terrains Trouver les coefficient

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+import os
+
 sols = []
 typesVegetation = ["Culture", "Pâturage", "Boisé", "Lac", "Marécage"]
 nombreSols = 0
+pourcentageRestant = 100.0
+
+os.system("cls")
+
+
+def print_reponses(nb, veg, pen, clas, pourc, fin):
+    if nb is not None:
+        print("********** Sol #" + str(nb) + " **********")
+    print("Végétation: " + typesVegetation[veg - 1])
+    if pen is not None:
+        print("Pente: " + str(pen) + "%")
+    else:
+        if fin:
+            print("Pente: N/A")
+    if clas is not None:
+        print("Classification hydrologique: " + clas)
+    else:
+        if fin:
+            print("Classification hydrologique: N/A")
+    if pourc is not None:
+        print("Pourcentage du terrain: " + str(pourc) + "%")
+
 
 # Aire totale
 while True:
@@ -13,15 +37,18 @@ while True:
     except Exception:
         print("\nNOPE, PAS UNE OPTION!\nRÉESSAYE DUMBASS\n")
         continue
+os.system("cls")
 
 while True:
+    # Réinitialisation des variables
     vegetation = None
     pente = None
     classHydro = None
     pourcentageSol = None
+
     # Numéro du sol
     nombreSols += 1
-    print("********** Sol " + str(nombreSols) + " **********")
+    print("********** Sol #" + str(nombreSols) + " **********")
 
     # Végétation
     while True:
@@ -34,6 +61,8 @@ while True:
         except Exception:
             print("\nNOPE, PAS UNE OPTION!\nRÉESSAYE DUMBASS\n")
             continue
+    os.system("cls")
+    print_reponses(nombreSols, vegetation, pente, classHydro, pourcentageSol, False)
 
     if vegetation <= 3:
         # Pente
@@ -47,13 +76,17 @@ while True:
             except Exception:
                 print("\nNOPE, PAS UNE OPTION!\nRÉESSAYE DUMBASS\n")
                 continue
+        os.system("cls")
+        print_reponses(nombreSols, vegetation, pente, classHydro, pourcentageSol, False)
         # Classification Hydrologique
         while True:
-            classHydro = input("Quelle est la classification hydraulique du sol: ")
-            if classHydro.upper() not in ["A", "B", "C", "D"]:
+            classHydro = input("Quelle est la classification hydraulique du sol: ").upper()
+            if classHydro not in ["A", "B", "C", "D"]:
                 print("\nNOPE, PAS UNE OPTION!\nRÉESSAYE DUMBASS\n")
                 continue
             break
+        os.system("cls")
+        print_reponses(nombreSols, vegetation, pente, classHydro, pourcentageSol, False)
 
     # Pourcentage de l'aire totale occupée par ce sol
     while True:
@@ -61,21 +94,122 @@ while True:
         try:
             pourcentageSol = float(pourcentageSol)
             if not 0 < pourcentageSol <= 100:
+                print("\nNOPE, PAS UNE OPTION!\nRÉESSAYE DUMBASS\n")
                 raise Exception
-            break
+            if pourcentageRestant - pourcentageSol >= 0:
+                break
+            print("\nNOPE, PAS UNE OPTION!\nPOURCENTAGE ENTRÉ EST PLUS GRAND QUE POURCENTAGE DE TERRAIN RESTANT\nIL RESTE: " + str(pourcentageRestant) + "%\n")
+            raise Exception
         except Exception:
-            print("\nNOPE, PAS UNE OPTION!\nRÉESSAYE DUMBASS\n")
             continue
+    os.system("cls")
+
+    match vegetation:
+        case 1:
+            if pente < 3:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.22
+                    case 'B':
+                        coefficient = 0.36
+                    case 'C':
+                        coefficient = 0.47
+                    case 'D':
+                        coefficient = 0.51
+            elif pente > 8:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.32
+                    case 'B':
+                        coefficient = 0.51
+                    case 'C':
+                        coefficient = 0.67
+                    case 'D':
+                        coefficient = 0.73
+            else:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.25
+                    case 'B':
+                        coefficient = 0.43
+                    case 'C':
+                        coefficient = 0.59
+                    case 'D':
+                        coefficient = 0.67
+        case 2:
+            if pente < 3:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.08
+                    case 'B':
+                        coefficient = 0.17
+                    case 'C':
+                        coefficient = 0.34
+                    case 'D':
+                        coefficient = 0.43
+            elif pente > 8:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.20
+                    case 'B':
+                        coefficient = 0.39
+                    case 'C':
+                        coefficient = 0.56
+                    case 'D':
+                        coefficient = 0.64
+            else:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.10
+                    case 'B':
+                        coefficient = 0.25
+                    case 'C':
+                        coefficient = 0.43
+                    case 'D':
+                        coefficient = 0.51
+        case 3:
+            if pente < 3:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.04
+                    case 'B':
+                        coefficient = 0.15
+                    case 'C':
+                        coefficient = 0.29
+                    case 'D':
+                        coefficient = 0.37
+            elif pente > 8:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.11
+                    case 'B':
+                        coefficient = 0.26
+                    case 'C':
+                        coefficient = 0.43
+                    case 'D':
+                        coefficient = 0.51
+            else:
+                match classHydro:
+                    case 'A':
+                        coefficient = 0.07
+                    case 'B':
+                        coefficient = 0.19
+                    case 'C':
+                        coefficient = 0.34
+                    case 'D':
+                        coefficient = 0.43
+        case 4:
+            coefficient = 0.95
+        case 5:
+            coefficient = 0.05
 
     nouveauSol = [vegetation, pente, classHydro, pourcentageSol]
     sols.append(nouveauSol)
-    if pente is None:
-        pente = "N/A"
-    if classHydro is None:
-        classHydro = "N/A"
-    print("Nouveau sol ajouté:\n"
-          "Végétation: " + typesVegetation[vegetation - 1] + "\n"
-          "Pente: " + str(pente) + "\n"
-          "Classification Hydrologique: " + str(classHydro) + "\n"
-          "Pourcentage du sol: " + str(pourcentageSol))
-    print(sols)
+    pourcentageRestant -= pourcentageSol
+    print("********** Sol #" + str(nombreSols) + " ajouté au calcul **********")
+    print_reponses(None, vegetation, pente, classHydro, pourcentageSol, True)
+    if pourcentageRestant <= 0:
+        break
+
+print("Caalcuuuuul")
+
